fa2b48e0b288bfea290ed8c23ac81111c8b371d4
nadhifikbarw <nadhifikbarw@gmail.com>
2025-07-29 02:50:59 +0700

    exercise 0204

---
+++ exercises/0204/uninit_var.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    int myint1, myint2, myint3, myint4, myint5, myint6;
+    float myfloat1, myfloat2, myfloat3, myfloat4, myfloat5, myfloat6;
+
+    printf("myint1=%d\n", myint1);
+    printf("myint2=%d\n", myint2);
+    printf("myint3=%d\n", myint3);
+    printf("myint4=%d\n", myint4);
+    printf("myint5=%d\n", myint5);
+    printf("myint6=%d\n", myint6);
+
+    printf("myfloat1=%f\n", myfloat1);
+    printf("myfloat2=%f\n", myfloat2);
+    printf("myfloat3=%f\n", myfloat3);
+    printf("myfloat4=%f\n", myfloat4);
+    printf("myfloat5=%f\n", myfloat5);
+    printf("myfloat6=%f\n", myfloat6);
+
+    return 0;
+}
