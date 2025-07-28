33d0f6d29c37b1018071ee43463f2ecee2716979
nadhifikbarw <nadhifikbarw@gmail.com>
2025-07-29 06:10:21 +0700

    fix 0202

--- projects/0202/vsphere_10.c
+++ projects/0202/vsphere_10.c
@@ -5,12 +5,11 @@ int main(void) {
     float fr = 4.0f/3.0f, buggy_fr = 4/3;
     float pi = 3.14f;
 
-    float radius;
-    printf("Radius: ");
-    scanf("%f", &radius);
+    float radius = 10.0f;
 
-    printf("Sphere volume: %f\n", fr * pi * radius * radius * radius);
-    printf("Sphere volume (buggy): %f\n", buggy_fr * pi * radius * radius * radius);
+    printf("Sphere volume (m): %f\n", fr * pi * radius * radius * radius);
+    printf("Sphere volume (m) (buggy): %f\n", buggy_fr * pi * radius * radius * radius);
+
     printf("fr=%f ; buggy_fr=%f\n", fr, buggy_fr);
     return 0;
 }
