c252d1b4009035ca83b2340024332760490a94b7
nadhifikbarw <nadhifikbarw@gmail.com>
2025-08-02 08:36:08 +0700

    exercise 0503

---
+++ exercises/0503/1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    int i, j, k;
+
+    i = 3; j = 4; k = 5;
+    printf("(a) %d\n", i < j || ++j < k);
+    printf("(a) %d %d %d\n", i, j, k);
+
+    i = 7; j = 8; k = 9;
+    printf("(b) %d\n", i - 7 && j++ < k);
+    printf("(a) %d %d %d\n", i, j, k);
+
+    i = 7; j = 8; k = 9;
+    printf("(c) %d\n", (i = j) || (j = k));
+    printf("(a) %d %d %d\n", i, j, k);
+
+    i = 1; j = 1; k = 1;
+    printf("(d) %d\n", ++i || ++j && ++k);
+    printf("(a) %d %d %d\n", i, j, k);
+
+    return 0;
+}
---
+++ exercises/0503/README.md
@@ -0,0 +1,13 @@
+# Solutions
+
+Self reasoned (then checked against `1.c`)
+```
+(a) 1
+(a) 3 4 5
+(b) 0
+(b) 7 8 9
+(c) 1
+(c) 8 8 9
+(d) 1
+(d) 2 1 1
+```
